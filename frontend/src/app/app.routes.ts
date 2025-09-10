2b097a94caaeee80482d46eaf2b828556b916809
Hadroon <gabor.muranyi@gmail.com>
2025-09-10 14:57:32 +0200

    how it works component

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,10 +1,15 @@
 import { Routes } from '@angular/router';
 import { Home } from './home/home';
+import { HowItWorks } from './how-it-works/how-it-works';
 
 export const routes: Routes = [
   {
     path: '',
     component: Home
+  },
+  {
+    path: 'how-it-works',
+    component: HowItWorks
   },
   {
     path: '**',
