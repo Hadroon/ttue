63f375d586c3727197370ee05c74760beea031a5
Hadroon <gabor.muranyi@gmail.com>
2025-09-10 15:24:27 +0200

    header component in shared

--- frontend/src/app/how-it-works/how-it-works.ts
+++ frontend/src/app/how-it-works/how-it-works.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
-import { Header } from '../header/header';
+import { Header } from '../shared/components';
 
 @Component({
   selector: 'app-how-it-works',
