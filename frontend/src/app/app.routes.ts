009a64152cd682dce98903700c17213b242187c7
Hadroon <gabor.muranyi@gmail.com>
2025-10-30 09:39:49 +0100

    test component

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { Home } from './home/home';
 import { HowItWorks } from './how-it-works/how-it-works';
+import { Test } from './test/test';
 
 export const routes: Routes = [
   {
@@ -10,6 +11,10 @@ export const routes: Routes = [
   {
     path: 'how-it-works',
     component: HowItWorks
+  },
+  {
+    path: 'test',
+    component: Test
   },
   {
     path: '**',
